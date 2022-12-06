8f8196251f100ea3ec17e3b9b5260d04c7f4a943
Somin <116507574+sominkimm@users.noreply.github.com>
2022-12-06 15:57:06 +0900

    Create Frontend part

---
+++ Frontend/vue.config.js
@@ -0,0 +1,16 @@
+const { VUE_APP_SERVER } = process.env
+
+module.exports = {
+  devServer: {
+    overlay: false,
+    proxy: {
+      "/serverApi": {
+        target: VUE_APP_SERVER,
+        changeOrigin: true,
+        pathRewrite: {
+          "^/serverApi": "",
+        },
+      },
+    },
+  },
+}
