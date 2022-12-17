368dbf6589c93702e8d976d783b8707dbdd10dbc
HJ <skwo777@hufs.ac.kr>
2022-12-17 18:36:04 +0900

    Merge Backend

--- Backend/models/mqtts.js
+++ Backend/models/mqtts.js
@@ -1,18 +1,14 @@
-// const { text } = require('body-parser');
 const mqtt = require('mqtt');
-// const { connection } = require('mysql2');
-// const mysql = require('mysql2');
 const db = require('./index');
 
 const { Edukit, sequelize } = require('./index');
 
-// const conn = mysql.createConnection(connection);
-
-let changeDetector = false; // 시작정지 감지  test 한다고 투루로 함. 원래 펄스
+let changeDetector = false; // 시작정지 감지
 let emergencyDetector = false; // 비상정지 감지
 let prossecing = false; // 작업중 표시변수
-
-const client = mqtt.connect('mqtt:192.168.0.79:1555');
+let cnt = 0;
+// let check = -1;
+const client = mqtt.connect('mqtt:192.168.0.79:2555');
 
 client.on('connect', () => {
   client.subscribe('myEdukit', (err) => {
@@ -32,70 +28,91 @@ client.on('connect', () => {
 
 client.on('message', async (myEdukit, message) => {
   const obj = JSON.parse(message.toString());
-  // console.log('her1e');
+  // if (check !== obj.Wrapper[31].value) { // 현재생산량이 달라지면
+  // for (let i = 0; i < 10; i++) {
+  client.publish('testest', obj.Wrapper[31].value); // 현재생산량 mqtt 발행
+  // }
 
-  // 비상정지 종료시
+  // check = obj.Wrapper[31].value; // 체크포인트
+  // }
+
+  // // 비상정지 시작시    1초 뒤 인식
+  if (obj.Wrapper[27].value === false && emergencyDetector === false) {
+    setTimeout(() => {
+      cnt += 1;
+      if (obj.Wrapper[27].value === false && cnt === 1) {
+        if (prossecing === true) { // 작업중 비상정지의 경우
+          Edukit.max('id').then((max) => {
+            Edukit.update({
+              eStop: 'O', // 비상정지 여부를 업데이트
+              estopRuntime: Date.now(), // 비상정지 시작시간 업데이트
+            }, { where: { id: max } });
+          });
+        } else {
+          // 작업중이 아닌 비상정지의 경우
+          Edukit.create({ // 작업생성 후  비상정지 여부 표시
+            eStop: 'O',
+            pdStartTime: Date.now(),
+            estopRuntime: Date.now(), // 비상정지 시작시간 기록시작
+          });
+        }
+        emergencyDetector = true;
+        setTimeout(() => { cnt = 0; }, 1000);
+      }
+    }, 1000);
+  }
+
+  // 비상정지 종료시  1초 뒤 인식
 
   if (obj.Wrapper[27].value === true && emergencyDetector === true) {
-    // 비상정지에 의한 작업종료시 해당작업의 비상정지해제 시간 분기점 필요
-    if (prossecing === true) {
-      console.log('작업중 비상정지의 경우');
-    } else {
-      console.log('작업중이 아닌 비상정지의 경우');
-    }
-    console.log('비상정지 기록정지시점');
-    emergencyDetector = false;
-    prossecing = false;
+    setTimeout(() => {
+      if (emergencyDetector === true) {
+        Edukit.max('id').then((max) => {
+          Edukit.update({
+            pdEndTime: Date.now(),
+            estopCleartime: Date.now(), // 비상정지 종료시간 업데이트
+          }, { where: { id: max } });
+        });
+        emergencyDetector = false;
+        prossecing = false;
+      }
+    }, 1000);
   }
 
-  // // 비상정지 시작시
-  if (obj.Wrapper[27].value === false && emergencyDetector === false) {
-    if (prossecing === true) {
-      console.log('작업중 비상정지의 경우');
-    } else {
-      console.log('작업중이 아닌 비상정지의 경우');
-    }
-    console.log('비상정지 기록시작시점');
-    emergencyDetector = true;
-  }
   // 작업 시작 여부 변화 감지 (작업 시작시 한번만 실행)
 
   if (changeDetector !== obj.Wrapper[6].value && obj.Wrapper[6].value === true) {
     changeDetector = obj.Wrapper[6].value;
-    console.log('here333');
     const testMake = await Edukit.create({
-      eStop: 'here',
+      eStop: 'X',
       pdStartTime: Date.now(),
     });
-    // test: id 추출
-    if (testMake) {
-      const result = await sequelize.query('SELECT id from edukits');
-      console.log(result);
-    }
-    console.log('data');
+    // const tset = await Edukit.findOne();
+    // console.log("Jane's auto-generated ID:", tset);
   }
 
   // // 작업이 끝나면 한번만 실행
   if (changeDetector !== obj.Wrapper[6].value && obj.Wrapper[31].value !== 0) {
-    const nowOutput = obj.Wrapper[31].value;
-    const goods = obj.Wrapper[32].value;
-    const detective = nowOutput - goods;
-    console.log('현재 생산량 : %d 현재 양품 생산량 : %d, 현재 불량품 : %d', nowOutput, goods, detective);
+    const nowOutput = obj.Wrapper[31].value; // 1호기 카운트, 총 생산량
+    const goods = obj.Wrapper[32].value; // 2호기 카운트, 양품 생산량
+    const detective = nowOutput - goods; // 총생산량 - 양품생산량, 불량품
     changeDetector = obj.Wrapper[6].value;
+    // const resents = await sequelize.query('SELECT id FROM edukits ORDER BY id DESC LIMIT 1');
 
-    // const b = Edukit.update({
-    //   firOutput: nowOutput,
-    //   pdEndTime: Date.now(),
-    //   thrGoodset: goods,
-    //   gappyProduct: detective,
-    // });
-
-    // console.log('b-data', b);
+    Edukit.max('id').then((max) => {
+      Edukit.update({ // 계산된 작업량 DB 업데이트
+        firOutput: nowOutput,
+        pdEndTime: Date.now(),
+        thrGoodset: goods,
+        gappyProduct: detective,
+      }, {
+        where: { id: max }, // 가장 최근 작업에 업로드
+      });
+    });
 
     // 비상정지에 의한 작업종료시
     if (obj.Wrapper[27].value === false) {
-      console.log('비상정지 여부표시');
-      prossecing = true;
+      prossecing = true; // 작업중 표시
     }
   }
 });
